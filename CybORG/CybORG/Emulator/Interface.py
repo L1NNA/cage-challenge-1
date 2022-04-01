47a17323992bb93705e25e9d09d9a99cfb3a56a8
Henry Tsui <thehenrytsui@gmail.com>
2022-04-01 02:26:20 -0400

    Create Interface.py

---
+++ CybORG/CybORG/Emulator/Interface.py
@@ -0,0 +1,14 @@
+# Copyright DST Group. Licensed under the MIT license.
+from ipaddress import IPv4Address, IPv4Network
+
+from CybORG.Emulator.Entity import Entity
+
+# class Interface creates a object interface with parameter name, ip address and assigns it to a subnet
+# def __init__(): constructor for object, assigns attributes
+# def get_state(self): returns the attributes within a dictionary
+class Interface(Entity):
+    def __init__(self):
+        pass
+
+    def get_state(self):
+        pass
